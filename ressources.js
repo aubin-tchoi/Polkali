78fcfd99d1ade5ab512bf52cbb8f9182de3fc177
aubin-tchoi <aubin.tchoi@gmail.com>
2021-04-03 22:53:25 +0200

    minor fix

--- ressources.js
+++ ressources.js
@@ -57,4 +57,4 @@ function isNumeric(str) {
     if (typeof str != "string") {return false;} // Only processes strings
     return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
       !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
-  }
+}
