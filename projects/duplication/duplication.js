06d8d2aa212b3cffcd6dadd0c6995c00a17dd417
aubin-tchoi <aubin.tchoi@gmail.com>
2021-04-17 15:43:11 +0200

    duplication updated

--- projects/duplication/duplication.js
+++ projects/duplication/duplication.js
@@ -1,24 +1,33 @@
 /* Si vous avez des questions à propos de ce script contactez Aubin Tchoï (Directeur Qualité 022) */
 
-function duplicate_folder() {
-  const folder = DriveApp.getFolderById("1ZnkIXGncM7g5J23rVSEMZ-nOqu80itaM"),
-    destination = DriveApp.getFolderById("1qITNGvF2upvvh52z6bmq2f4G0eKbdlq5"),
-    dest_list = ["Antoine", "Mai-Linh", "Loïs", "Arnaud", "Téo", "Nathan", "Caroline", "Thibaut"];
-  dest_list.forEach(function (name) {
-    let newFolder = destination.createFolder(name);
-    let folders = folder.getFolders();
-    while (folders.hasNext()) {
-      let subFolder = folders.next();
-      let newSubFolder = newFolder.createFolder(subFolder.getName());
-      let files = subFolder.getFiles();
-      while (files.hasNext()) {
-        try {
-          var targetFile = files.next();
-          targetFile.makeCopy(targetFile.getName(), newSubFolder);
-        } catch (e) {
-          Logger.log(`Error with file ${targetFile.getName()}`);
-        }
-      }
-    }
-  })
+// IDs of the 2 folders to duplicate
+const SOURCE_ID = "",
+  TARGET_ID = "";
+
+// Copying all the files from source to target (source and target are instances of Folder class)
+function copyFiles(source, target) {
+  let files = source.getFiles();
+  while (files.hasNext()) {
+    let file = files.next();
+    file.makeCopy(target);
+  }
+}
+
+// Recursive function to copy all files from source to target and then iterate on each subFolder
+function duplicateFolder(source, target) {
+  copyFiles(source, target);
+  // Looping on each subFolder (no need for a stopping condition, the hasNext method provides it)
+  let folders = folder.getFolders();
+  while (folders.hasNext()) {
+    let subFolder = folders.next(),
+      targetSubFolder = target.createFolder(subFolder.getName());
+    duplicateFolder(subFolder, targetSubFolder);
+  }
+}
+
+// Main function
+function main() {
+  const source = DriveApp.getFolderById(sourceID),
+    target = DriveApp.getFolderById(targetID);
+  duplicateFolder(source, target);
 }
